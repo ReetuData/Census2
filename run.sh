9667ee48f86d626fa2b0a78db5589b40f42a9087
reetudeepu <12reetu12@gmail.com>
2020-11-04 20:21:34 -0800

    Census Data Processor
    Census Data Processor

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+python ./src/DataProcessor.py ./input/censustract-00-10.csv ./output/report.csv
---
+++ src/DataProcessor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import csv
+import sys
+from csv import DictReader
+ 
+class Census_data_processor:
+
+    def __init__(self, inputFilePath, outputFilePath):
+        self.inputFilePath = inputFilePath
+        self.outputFilePath = outputFilePath
+
+    def tryfloat(_self, val):
+        try:
+            return float(val)
+        except ValueError:
+            return 0
+        
+    def generateOutput(self, dataDictionary):
+        with open(self.outputFilePath, 'w') as file:
+            writer = csv.writer(file)
+            for key, val in dataDictionary.items():
+                writer.writerow(val)
+
+    def processCensusData(self):
+        dataList = []
+        with open(self.inputFilePath, 'r') as read_obj:
+            csv_dict_reader = DictReader(read_obj)
+            for row in csv_dict_reader:
+                dataList.append([row['CBSA09'],row['CBSA_T'],row['POP00'],row['POP10'],row['PPCHG']])
+            
+        areaCountDictionary = {}
+        for l in dataList:
+            if (l[0] not in areaCountDictionary):
+                areaCountDictionary[l[0]] = [l[0], "%s"%l[1], 1, float(l[2]), float(l[3]), float(l[4])]
+            else:
+                areaCountDictionary[l[0]][2] = areaCountDictionary[l[0]][2] + 1;
+                areaCountDictionary[l[0]][3] = areaCountDictionary[l[0]][3] + float(l[2]);
+                areaCountDictionary[l[0]][4] = areaCountDictionary[l[0]][4] + float(l[3]);
+                areaCountDictionary[l[0]][5] = areaCountDictionary[l[0]][5] + self.tryfloat(l[4]);
+
+        for key, val in areaCountDictionary.items():
+            val[5] = round(val[5]/val[2],2)
+
+        self.generateOutput(areaCountDictionary)
+        
+processor = Census_data_processor(sys.argv[1], sys.argv[2])
+processor.processCensusData()
